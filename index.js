747b13b3599f5357b66c189a34bffad546374098
Wilfried <wilfried.djopa@gmail.com>
2022-01-31 11:19:04 +0100

    updates

--- index.js
+++ index.js
@@ -3,29 +3,33 @@ window.cnt2 = 0
 
 // Desired days is the list
 // FORMAT : DDMMYYYY 
-let desired_days = ["19112021",
-    "20112021",
-    "21112021",
-    "22112021",
-    "23112021",
-    "24112021",
-    "25112021",
-    "26112021",
-    "27112021",
-    "28112021",
-    "29112021",
-    "30112021",
-    "01122021",
-    "02122021",
-    "03122021",
-    "04122021",
-    "05122021",
-    "06122021",
-    "07122021",
-    "08122021",
-    "09122021",
-    "10122021",
+let desired_days = [
+    "01022022",
+    "02022022",
+    "03022022",
+    "04022022",
+    "05022022",
+    "06022022",
+    "07022022",
+    "08022022",
+    "09022022",
+    "10022022",
+    "11022022",
+    "12022022",
+    "13022022",
+    "14022022",
+    "15022022",
+    "16022022",
+    "17022022",
+    "18022022",
+    "19022022",
+    "20022022",
+    "21022022",
+    "22022022",
 ]
+
+
+// TO RECEIVE NOTIFICATIONS
 
 
 function clearAllInterval() {
@@ -36,13 +40,14 @@ function clearAllInterval() {
 function notify_me(available_date = "UNKNOW") {
     let message = "🚨🚨🚨 ALERTE RDV Disponible le : " + available_date + " Lien : https://pastel.diplomatie.gouv.fr/rdvinternet/html-4.02.00/frameset/frameset.html?lcid=1&sgid=318&suid=1"
     // ADD A WEBSERVICE TO SEND AN EMAIL OR A TEXT MESSAGE HERE
-    // fetch("https://dashboard.genuka.com/api/2021-05/notify/telegram/355?message=" + message)
+    fetch("https://preprod.genuka.com/api/2021-05/notify/telegram/2?message=" + message, { mode: "no-cors" });
+    const notification = new Notification("🚨🚨🚨 ALERTE RDV Disponible", { body: message });
 }
 
 
 async function get_horaires() {
     // J'ai obtenu pp.idPage en regardant dans l'onglet Sources > CONTENuWIN > RDVInternet > RDV/prise > PrendreRDV.js : ligne 704
-    let response = await fetch("https://pastel.diplomatie.gouv.fr/rdvinternet/flux/protected/RDV/prise/horaires.xml?idPage=" + pp.idPage)
+    let response = await fetch("https://pastel.diplomatie.gouv.fr/rdvinternet/flux/protected/RDV/prise/horaires.xml?idPage=" + parent.parent.idPage)
     response = await response.text()
     // console.log(response)
     const parser = new DOMParser();
@@ -98,14 +103,14 @@ async function get_horaires() {
 
 function script_precedent() {
     if (!window.alert_activated && document.querySelector) {
-        notify_me()
+   //     notify_me()
     }
-    tabElementForm[0].precedent()
+    // document.querySelector("#boutonPrecedent").click()
     window.alert_activated = false;
 }
 
 function script_suivant() {
-    tabElementForm[0].suivant()
+    // document.querySelector("#boutonSuivant").click()
 }
 
 window.orchestration = () => {
@@ -120,8 +125,11 @@ window.alert = function (message) {
     window.alert_activated = true;
     console.log("Heure : " + (new Date()) + " Message : " + message)
     window.cnt2++
-    if (window.cnt2 % 17 === 0)
-        fetch("https://dashboard.genuka.com/api/2021-05/notify/telegram/2?message=" + "Heure : " + (new Date()) + " Message : " + message)
+    if (window.cnt2 % 17 === 0){
+        const notification = new Notification("Heure : " + (new Date()) , { body: message });
+
+        fetch("https://preprod.genuka.com/api/2021-05/notify/telegram/2?message=" + "Heure : " + new Date() + " Message : " + message, { mode: "no-cors" });
+    }
     console.log(window.alert_activated, message)
 };
 
@@ -133,21 +141,21 @@ function fill_form(date) {
 
         compTableau.appelAction(0, 0)
         setTimeout(() => {
-            tabElementForm[0].suivant()
+            // document.querySelector("#boutonSuivant").click()
         }, 200)
         setTimeout(() => {
             // Sexe pas pris en compte
-            document.querySelector("#nom").value = "REMPLIR LES CHAMPS"
-            document.querySelector("#nomnaiss").value = "REMPLIR LES CHAMPS"
-            document.querySelector("#prenoms").value = ""
-            document.querySelector("#neen1").value = "DD"
-            document.querySelector("#neen2").value = "MM"
-            document.querySelector("#neen3").value = "AAAA"
-            document.querySelector("#natactuelle").value = "CHECK COUNTRY_VALUE BY INSPECTING THE SELECT/ 34 FOR CAMEROON"
-            document.querySelector("#num").value = ""
-            document.querySelector("#telephone").value = "PHONE"
-            document.querySelector("#email").value = "EMAIL"
-            document.querySelector("#confirm_email").value = "EMAIL"
+            // document.querySelector("#nom").value = "REMPLIR LES CHAMPS"
+            // document.querySelector("#nomnaiss").value = "REMPLIR LES CHAMPS"
+            // document.querySelector("#prenoms").value = ""
+            // document.querySelector("#neen1").value = "DD"
+            // document.querySelector("#neen2").value = "MM"
+            // document.querySelector("#neen3").value = "AAAA"
+            // document.querySelector("#natactuelle").value = "CHECK COUNTRY_VALUE BY INSPECTING THE SELECT/ 34 FOR CAMEROON"
+            // document.querySelector("#num").value = ""
+            // document.querySelector("#telephone").value = "PHONE"
+            // document.querySelector("#email").value = "EMAIL"
+            // document.querySelector("#confirm_email").value = "EMAIL"
         }, 500)
     }, 200)
 }
